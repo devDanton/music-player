76c2ee4432314aacac62e5d1855e2d25f4210424
Danton da Rosa Abreu <77130559+devDanton@users.noreply.github.com>
2023-01-08 16:54:46 -0300

    interface

---
+++ script.js
@@ -0,0 +1,8 @@
+function toLandscape() {
+  const html = document.documentElement
+  html.classList.toggle('landscape')
+}
+function toSmall() {
+  const html = document.documentElement
+  html.classList.toggle('small')
+}
